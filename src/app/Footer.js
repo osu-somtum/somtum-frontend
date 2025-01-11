9f82f0a93c4a58eba457d95778165e0af47dc5fb
UnitieG <furries@duck.com>
2025-01-11 11:10:39 +0700

    first commit ;3

---
+++ src/app/Footer.js
@@ -0,0 +1,49 @@
+export default function Footer() {
+  return (
+    <footer className="bg-white rounded-lg shadow bg-gray-900/50 m-4 fixed bottom-0 left-0 right-0  border-gray-600">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <a
+            href="https://somtum.fun/"
+            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">osu!somtum</span>
+          </a>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li className="flex items-center">
+              <img
+                src="/github-mark.svg"
+                alt="GitHub logo"
+                className="h-4 w-4 me-2 filter invert"
+              />
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Github
+              </a>
+            </li>
+            <li className="flex items-center">
+              <img
+                src="/discord-mark-white.svg"
+                alt="Discord logo"
+                className="h-4 w-4 me-2 filter"
+              />
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                YouTube
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Twitter
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
---
+++ src/app/NavBar.js
@@ -0,0 +1,49 @@
+export default function NavBar() {
+  return (
+    <nav suppressHydrationWarning className="bg-white rounded-lg shadow bg-gray-900/50 m-4 fixed top-0 left-0 right-0  border-gray-600">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="flex items-center space-x-3 rtl:space-x-reverse">
+          <a href="https://somtum.fun">
+            <img src="https://pla-ra.xyz/static/images/somtum.png" className="h-8" alt="Logo" />
+          </a>
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700">
+            <li>
+              <a href="#" className="block py-2 px-3 text-white rounded md:bg-transparent md:text-emerald-300 md:p-0" aria-current="page">Home</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-white rounded md:hover:bg-transparent md:hover:text-emerald-300 md:p-0 hover:bg-transparent">Leaderboard</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-white rounded md:hover:bg-transparent md:hover:text-emerald-300 md:p-0 hover:bg-transparent">Top Plays</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-white rounded md:hover:bg-transparent md:hover:text-emerald-300 md:p-0 hover:bg-transparent">Guilds</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-white rounded md:hover:bg-transparent md:hover:text-emerald-300 md:p-0 hover:bg-transparent">Donation</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-white rounded md:hover:bg-transparent md:hover:text-emerald-300 md:p-0 hover:bg-transparent">Docs</a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex md:order-2">
+          <div className="relative hidden md:block mr-2">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+              </svg>
+              <span className="sr-only">Search icon</span>
+            </div>
+            <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm border border-gray-300 rounded-lg bg-gray-700/70 border-gray-600 placeholder-gray-400 text-white focus:border-blue-500" placeholder="Search..."/>
+          </div>
+          <button type="button" className="text-white focus:ring-2 focus:outline-none focus:ring-teal-500 font-medium rounded-lg text-base px-4 py-2 text-center bg-emerald-500 hover:bg-emerald-600 mb-2 md:mb-0 md:mr-2">Sign Up</button>
+          <button type="button" className="text-white focus:ring-2 focus:outline-none focus:ring-teal-500 font-medium rounded-lg text-base px-4 py-2 text-center bg-emerald-500 hover:bg-emerald-600">Log in</button>
+          <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
