a9b8c3643fbefdb32a0a35e2e06a539f45a6d406
UnitieG <furries@duck.com>
2025-01-14 16:49:17 +0700

    fixs

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import NavBar from "../components/NavBar";
-import Footer from "../components/Footer";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
