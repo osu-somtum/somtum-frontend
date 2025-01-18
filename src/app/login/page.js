78fda8cb3d83cba2ad5a22c6816545bc299d4d5d
UnitieG <furries@duck.com>
2025-01-18 19:51:18 +0700

    login page

---
+++ src/app/login/page.js
@@ -0,0 +1,53 @@
+'use client';
+
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+
+export default function RegisterPage() {
+
+
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-mali)]">
+            <NavBar />
+            <main className="flex flex-col gap-8 row-start-2 items-center w-full">
+                <div className="h-full w-auto border rounded-lg shadow bg-gray-900/50 border-gray-700/50 p-8">
+                    <div className="flex flex-col gap-8 row-start-2 items-center p-4">
+                        <p5 className="text-3xl font-bold text-white mb-8">Login</p5>
+                        <form className="flex flex-col gap-4 w-full max-w-md px-4">
+                            <input
+                                type="text"
+                                name="username"
+                                placeholder="Username"
+                                className="p-2 border rounded-lg bg-slate-500/50 border-slate-500/50 border text-white"
+                                required
+                            />
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                className="p-2 border rounded-lg bg-slate-500/50 border-slate-500/50 border text-white"
+                                required
+                            />
+                            <input
+                                type="password"
+                                name="confirmPassword"
+                                placeholder="Confirm Password"
+                                className="p-2 border rounded-lg bg-slate-500/50 border-slate-500/50 border text-white"
+                                required
+                            />
+                            <p className="text-sm text-white">Don't have an account? <a className="text-blue-400" href="/register">Register Here.</a></p>
+                            <button type="submit" className="bg-slate-500/50 text-white p-2 rounded-md hover:bg-slate-700/50 transition-colors">
+                                Login
+                            </button>
+                            <button type="submit" className="bg-slate-500/50 text-white p-2 rounded-md hover:bg-slate-700/50 transition-colors">
+                                Forgot Password
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </main>
+            <Footer />
+        </div>
+    );
+}
+
